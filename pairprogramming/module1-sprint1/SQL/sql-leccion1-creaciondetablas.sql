fc8f68dbb9e9fdcac5e57df815694ba51fae1546
solkiria <sonia.ruiz.p31@gmail.com>
2023-07-02 20:17:41 +0200

    update sql code

--- pairprogramming/module1-sprint1/SQL/sql-leccion1-creaciondetablas.sql
+++ pairprogramming/module1-sprint1/SQL/sql-leccion1-creaciondetablas.sql
@@ -1,49 +1,49 @@
 # Crear base de datos desde cero:
-create schema tienda_zapatillas;
-use tienda_zapatillas;
+CREATE SCHEMA tienda_zapatillas;
+USE tienda_zapatillas;
 
 #Creamos la tabla Zapatillas especificando para cada columna sus características:
-create table Zapatillas (
-id_zapatilla int auto_increment not null,
-modelo varchar(45) not null,
-color varchar(45) not null,
-primary key (id_zapatilla)
+CREATE TABLE Zapatillas (
+id_zapatilla INT AUTO_INCREMENT NOT NULL,
+modelo VARCHAR(45) NOT NULL,
+color VARCHAR(45) NOT NULL,
+PRIMARY KEY (id_zapatilla)
 );
 
 # Creamos la tabla Clientes especificando para cada columna sus características:
-create table Clientes (
-id_cliente int auto_increment not null,
-nombre varchar(45) not null,
-numero_telefono int(9) not null,
-email varchar(45) not null,
-direccion varchar(45) not null,
-ciudad varchar(45),
-provincia varchar(45) not null,
-pais varchar(45) not null,
-codigo_postal varchar(45) not null,
-primary key (id_cliente)
+CREATE TABLE Clientes (
+id_cliente INT AUTO_INCREMENT NOT NULL,
+nombre VARCHAR(45) NOT NULL,
+numero_telefono INT(9) NOT NULL,
+email VARCHAR(45) NOT NULL,
+direccion VARCHAR(45) NOT NULL,
+ciudad VARCHAR(45),
+provincia VARCHAR(45) NOT NULL,
+pais VARCHAR(45) NOT NULL,
+codigo_postal VARCHAR(45) NOT NULL,
+PRIMARY KEY (id_cliente)
 );
 
 # Creamos tabla empleadosespecificando para cada columna sus características:
 create table Empleados (
-id_empleado int auto_increment not null,
-nombre varchar(45) not null,
-tienda varchar(45) not null,
-salario int,
-fecha_incorporacion date not null,
-primary key (id_empleado)
+id_empleado INT AUTO_INCREMENT NOT NULL,
+nombre VARCHAR(45) NOT NULL,
+tienda VARCHAR(45) NOT NULL,
+salario INT,
+fecha_incorporacion DATE NOT NULL,
+PRIMARY KEY (id_empleado)
 );
 
 # Creamos la tabla Facturas especificando para cada columna sus características:
 create table Facturas (
-id_factura int auto_increment not null,
-numero_factura varchar(45) not null,
-fecha date not null,
-id_zapatilla int not null,
-id_empleado int not null,
-id_cliente int not null,
-primary key (id_factura),
-constraint fk_zapatillas foreign key (id_zapatilla) references Zapatillas (id_zapatilla),
-constraint fk_empleados foreign key (id_empleado) references Empleados (id_empleado),
-constraint fk_clientes foreign key (id_cliente) references Clientes (id_cliente)
+id_factura INT AUTO_INCREMENT NOT NULL,
+numero_factura VARCHAR(45) NOT NULL,
+fecha date NOT NULL,
+id_zapatilla INT NOT NULL,
+id_empleado INT NOT NULL,
+id_cliente INT NOT NULL,
+PRIMARY KEY (id_factura),
+CONSTRAINT fk_zapatillas FOREIGN KEY (id_zapatilla) REFERENCES Zapatillas (id_zapatilla),
+CONSTRAINT fk_empleados FOREIGN KEY (id_empleado) REFERENCES Empleados (id_empleado),
+CONSTRAINT fk_clientes FOREIGN KEY (id_cliente) REFERENCES Clientes (id_cliente)
 );
--- pairprogramming/module1-sprint1/SQL/sql-leccion2-alteraciondetablas.sql
+++ pairprogramming/module1-sprint1/SQL/sql-leccion2-alteraciondetablas.sql
@@ -1,18 +1,18 @@
 # Realizar correcciones en las tablas de la base de datos tienda_zapatilas:
 ##Modificar la tabla Zapatillas introduciendo la columna marca y la columna talla, especificando sus características:
 ALTER TABLE Zapatillas
-	ADD COLUMN marca varchar(45) not null, 
-	ADD COLUMN talla int not null;
+	ADD COLUMN marca VARCHAR(45) NOT NULL, 
+	ADD COLUMN talla INT NOT NULL;
 
 ##Modificar la tabla Empleados editando la columna salario:
 ALTER TABLE Empleados
-	MODIFY COLUMN salario float not null;
+	MODIFY COLUMN salario FLOAT NOT NULL;
 
 ##Modificar la tabla Clientes eliminando la columna pais y editando la columna codigo_postal:
 ALTER TABLE Clientes
 	DROP COLUMN pais,
-	MODIFY COLUMN codigo_postal int(5);
+	MODIFY COLUMN codigo_postal INT(5);
     
 ##Modificar la tabla Facturas introduciendo la columna total, especificando sus características:
 ALTER TABLE Facturas
-	ADD COLUMN total float not null;
+	ADD COLUMN total FLOAT NOT NULL;
